7713b17433b0fb60264573d49dcd7321b9bbe6d5
Azmatullah Syed <syedazmatullah90@gmail.com>
2026-02-17 01:50:36 -0500

    Create 01_create_table.sql

---
+++ SQL/01_create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE car_sale (
+    car_id TEXT,
+    "Date" DATE,
+    Customer_Name TEXT,
+    Gender TEXT,
+    Annual_Income NUMERIC,
+    Dealer_Name TEXT,
+    Company TEXT,
+    model TEXT,
+    engine TEXT,
+    transmission TEXT,
+    color TEXT,
+    Price NUMERIC,
+    dealer_No TEXT,
+    body_style TEXT,
+    phone TEXT,
+    dealer_region TEXT
+);
